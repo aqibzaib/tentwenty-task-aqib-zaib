4c61119a23c7e5a2cb835ac6c2bd1affc4626a61
aqib <chaqib4u@gmail.com>
2025-05-16 18:12:26 +0500

    chnage postion of component folder

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,8 @@
 import Image from "next/image";
-import HeroBanner from "./components/HeroBanner";
+import HeroBanner from "../../components/HeroBanner";
 
 export default function Home() {
   return (
-    
     <div className="h-[100vh] overflow-hidden">
       <HeroBanner />
     </div>
